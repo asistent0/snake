8111f43b0273d2f39dbb64fb21f6654b22e10561
asistent0 <asistent0@github.com>
2015-11-24 13:37:09 +0300

    ЗАвершение игры, уход от препятсвий, добыть еду

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,18 +13,12 @@ namespace Snake
         {
             Console.SetBufferSize(80, 25);
 
-            HorizontalLine line1 = new HorizontalLine(1, 78, 1, '+');
-            HorizontalLine line2 = new HorizontalLine(1, 78, 24, '+');
-            VerticalLine line3 = new VerticalLine(1, 24, 1, '+');
-            VerticalLine line4 = new VerticalLine(1, 24, 78, '+');
-            line1.Drow();
-            line2.Drow();
-            line3.Drow();
-            line4.Drow();
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Drow();
+            snake.Draw();
 
             FoodCreator foodCreator = new FoodCreator(80, 25, '$');
             Point food = foodCreator.CreateFood();
@@ -32,6 +26,11 @@ namespace Snake
 
             while (true)
             {
+                if(walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+
                 if(snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
